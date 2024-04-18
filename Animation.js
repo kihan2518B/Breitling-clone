d772e181e8c9f35f5ddd84f98eb73430980aa9f3
kihan2518B <kp648027@gmail.com>
2024-04-18 17:38:16 +0530

    Animation in Navbar

---
+++ Animation.js
@@ -0,0 +1,41 @@
+gsap.registerPlugin()
+
+gsap.fromTo("#logo-1",
+    {
+        opacity: 0,
+        duration: 2,
+        x: -40,
+        delay: 4,
+    }, {
+    opacity: 10,
+    duration: 2,
+    x: 0,
+    delay: 4,
+})
+
+gsap.fromTo("#logo-3",
+    {
+        opacity: 0,
+        duration: 2,
+        x: 35,
+        delay: 4,
+    },
+    {
+        opacity: 10,
+        duration: 2,
+        x: 0,
+        delay: 4,
+
+    })
+
+gsap.fromTo(".nav-link",
+    {
+        opacity: 0,
+        duration: 3,
+        delay: 2
+    },
+    {
+        opacity: 1,
+        duration: 3,
+        delay: 2
+    })
