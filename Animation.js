38ca9103a4db7d15ce6ee4e5094b0832802e79b6
kihan2518B <kp648027@gmail.com>
2024-05-01 19:22:43 +0530

    landing page

--- Animation.js
+++ Animation.js
@@ -1,5 +1,29 @@
 gsap.registerPlugin()
 
+// gsap.fromTo(".fade-element",
+//     {
+//         opacity: 100,
+//     },
+//     {
+//         opacity: 0,
+//         duration: 4,
+//     })
+
+// const text = new Typed("#dots", {
+//     strings: ["..."],
+//     typeSpeed: 400, // Adjust typing speed (milliseconds)
+//     backSpeed: 80, // Adjust backspace speed (milliseconds)
+//     loop: true, // Loop the animation
+//     cursorChar: "",
+// });
+// const loader = document.querySelector(".fade-element")
+// setTimeout(() => {
+//     loader.classList.remove("flex");
+//     loader.classList.add("hidden");
+// }, 4000)
+
+// Main
+
 gsap.fromTo("#logo-1",
     {
         opacity: 0,
@@ -31,11 +55,23 @@ gsap.fromTo("#logo-3",
 gsap.fromTo(".nav-link",
     {
         opacity: 0,
-        duration: 3,
-        delay: 2
+        // duration: 2,
+        // delay: 2
     },
     {
-        opacity: 1,
-        duration: 3,
+        opacity: 10,
+        // duration: 2,
         delay: 2
     })
+
+// gsap.fromTo("#Text",
+//     {
+//         opacity: 0,
+//         duration: 3,
+//         delay: 2
+//     },
+//     {
+//         opacity: 1,
+//         duration: 3,
+//         delay: 3
+//     })
